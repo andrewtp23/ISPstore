5807b7cf96a9858295125c11452a05e1cb6155bc
andrewtp23 <atp23@zips.uakron.edu>
2017-02-16 14:50:43 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
 session_start();
-$db = mysql_connect("db1.cs.uakron.edu:3306","atp23","rooL7oog") or die("Failed to open connection to MySQL server.");
-mysql_select_db("ISP_atp23") or die("Unable to select database");
+$db = mysql_connect("URLOFDATABASE","USER","PASSWORD") or die("Failed to open connection to MySQL server.");
+mysql_select_db("DATABASENAME") or die("Unable to select database");
 ?>
